79f6cce96135c1dffc92234badf5b4652411f304
9534495797 <chaubeyankit927@gmail.com>
2023-07-27 06:43:21 +0530

    ok

---
+++ set.py
@@ -0,0 +1,50 @@
+# set1 = {2, 4, 6, 8, 10}
+# set2 = {1, 2, 3, 4, 5}
+# The intersection of set1 and set2 is {2, 4}.
+
+
+
+
+# Output: {2, 4}
+# x=input()
+# y=input()
+# z=set(x)
+# w=set(y)
+# intersection=z.intersection(w)
+# print(intersection)
+# The intersection() method returns a new set that contains only the elements that are common to both sets. In this case, the intersection of set1 and set2 is {2, 4}.
+
+
+
+
+# set1 = {2, 4, 6, 7, 8}
+# # set2 = {1, 2, 3, 4, 5}
+# # union=set1.union(set2)
+# # print(union)
+# x=list(set1)
+# # print(x)
+# x.reverse()
+# print(x)
+
+
+
+
+
+
+# x={1,2,3,4,5,6,7,8,9}
+# m=max(x)
+# n=min(x)
+# print(m)
+# print(n)
+
+# x={1,2,3,4,5}
+# y={2,4,6,8,7}
+# z=x.union(y)
+# print(z)
+# w=x.intersection(y)
+# print(w)
+
+x={1,3,2,4,5,6}
+print(len(x))
+print(max(x))
+print(min(x))
