d3f7a498299f86838036c676fa38f57b39112870
9534495797 <chaubeyankit927@gmail.com>
2023-07-27 06:42:32 +0530

    ok

---
+++ list.py
@@ -0,0 +1,91 @@
+# list=[3,5,4,4,1,2,7,8,9]
+# print(len(list))
+# print(max(list))
+# print(min(list))
+# list.insert(2,10)
+# print(list)
+# list.append(11)
+# print(list)
+# b=list.count(4)
+# print(b)
+
+# list.remove(3) remove work with value
+# print(list)
+
+# list.reverse()
+# print(list)
+
+# list.sort()
+# print(list)
+
+
+# list1=[3,5,4,4,1,2,7,8,9]
+# print(list(set(list1)))
+
+# list.pop(2)
+# print(list)  pop work with index
+
+# list.clear()  delete full list without arg
+# print(list)
+
+# a=[1,2,3,4,5]
+# b=[6,7,8,9,10]
+
+# # c=a+b
+# # print(c)
+# c=set(a)
+# d=set(b)
+# intersection=c.intersection(d)
+# print(list(intersection))
+
+# union=d.union(c)
+# print(list(union))
+
+
+# a=[8,2,3,9,5,6,6]
+# print(list(a))
+# a.sort()
+# print(a)
+# a.reverse()
+# print(a)
+# print(list(set(a)))
+
+# a.pop(6)
+# print(a)
+# a.insert(0,1)
+# print(a)
+# a.append(12)
+# print(a)
+# a.clear()
+# print(a)
+
+# print(set(a))
+
+
+# x=[1,3,2,4,5,5,7,9]
+# print(x)
+# print(max(x))
+# print(min(x))
+# x.append(0)
+# print(x)
+# x.insert(4,12)
+# print(x)
+# b=x.count(5)
+# print(b)
+# x.reverse()
+# print(x)
+# x.sort()
+# print(x)
+# x.reverse()
+# print(x)
+
+# x.remove(0)
+# print(x)
+# x.pop(4)
+# print(x)
+# x.clear()
+# print(x)
+a=[1,2,3,4,5]
+b=[6,7,8,9,10]
+c=a+b
+print(c)
