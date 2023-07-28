5ca19869571552a95d8df65635d4198db1a9dc89
9534495797 <chaubeyankit927@gmail.com>
2023-07-28 06:14:17 +0530

    ok

---
+++ input.py
@@ -0,0 +1,13 @@
+# x=int(input())
+# print(x)
+# print(type(x))
+# y=float(input())
+# print(y)
+# print(type(y))
+# z=str(input())
+# print(z)
+# print(type(z))
+
+x=int(input("1st value= "))
+y=int(input("2nd value= "))
+print(x+y,x-y,x*y,x/y,x//y,x%y)
