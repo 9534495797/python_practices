65bacc8a780b2a54d75ec919a4ff1d176219de5b
9534495797 <chaubeyankit927@gmail.com>
2023-07-29 07:11:10 +0530

    ok

---
+++ fibo.py
@@ -0,0 +1,11 @@
+def fibo(num):
+    if num==0:
+        return 0
+    elif num==1:
+        return 1
+    else:
+        return fibo(num-1)+ fibo(num-2)
+num=int(input())
+for i in range(num+1):
+    print(fibo(i))
+    
