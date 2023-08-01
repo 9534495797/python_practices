07ab6188797573037d7e99224eb52f312cf4bca4
9534495797 <chaubeyankit927@gmail.com>
2023-08-01 07:10:44 +0530

    ok

---
+++ loop.py
@@ -0,0 +1,33 @@
+# i=2
+# while i<=20:
+#     print(i)
+#     i+=2
+
+
+
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1
+
+
+# i=1
+# while i<=5:
+#     print(i*"*")
+#     i+=1
+
+
+# i=5
+# while i>1:
+#     print(i*"*")
+#     i-=1
+
+# i=2
+# while i<=10:
+#     print(i*"*")
+#     i+=2
+
+for i in range(10,20):
+    
+    print(i)
+    
