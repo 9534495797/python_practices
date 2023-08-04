75560455fdc24d49279fa1047dccf068a1087a6f
9534495797 <chaubeyankit927@gmail.com>
2023-08-04 07:30:45 +0530

    ok

---
+++ vowelidentiy.py
@@ -0,0 +1,13 @@
+x=str(input("word= "))
+if 'a' in x:
+        print("vowel")
+elif 'e' in x:
+      print("vowel")
+elif 'i' in x:
+      print("vowel")
+elif 'o' in x:
+      print("vowel")
+elif 'u' in x:
+      print("vowel")
+else:
+    print("constant")
