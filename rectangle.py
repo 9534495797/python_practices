6dd7fa40f12ab180b12d562764b6ea4004021037
9534495797 <chaubeyankit927@gmail.com>
2023-08-04 07:29:37 +0530

    ok

---
+++ rectangle.py
@@ -0,0 +1,8 @@
+a=int(input("1st side= "))
+b=int(input("2nd side= "))
+c=int(input("3rd side= "))
+d=int(input("4th side= "))
+if a==c and b==d:
+    print("yes rectangle")
+else:
+    print("it is not rectangle")
